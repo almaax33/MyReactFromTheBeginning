5088144c33132ee84208ede3823c248a180f44c8
Alma Garcia <lin_alma30@hotmail.com>
2020-02-24 18:23:36 -0600

    Restart from zero

--- section1/lecture15-practiceProps/app.js
+++
@@ -1,9 +0,0 @@
-ReactDOM.render(
-    <div className="row">
-        <Card data={data[0]}/>
-        <Card data={data[1]}/>
-        <Card data={data[2]}/>
-        <Card data={data[3]}/>  
-    </div>, 
-    document.getElementById('root')
-)
